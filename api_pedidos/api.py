6d475e20ef6e4f646c731fc3a5cab805f4e1699f
cassiobotaro <cassiobotaro@gmail.com>
2022-02-22 00:07:56 -0300

    Adiciona listagem de itens em um pedido

--- api_pedidos/api.py
+++ api_pedidos/api.py
@@ -1,9 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Request
+from fastapi.responses import JSONResponse
+from uuid import UUID
+from api_pedidos.esquema import Item
+from api_pedidos.excecao import PedidoNaoEncontradoError
+from http import HTTPStatus
 
 
 app = FastAPI()
 
 
+def recuperar_itens_por_pedido(identificacao_do_pedido: UUID) -> list[Item]:
+    pass
+
+
+@app.exception_handler(PedidoNaoEncontradoError)
+def tratar_erro_pedido_nao_encontrado(request: Request, exc: PedidoNaoEncontradoError):
+    return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content={"message": "Pedido não encontrado"})
+
+
 @app.get("/healthcheck")
 async def healthcheck():
     return {"status": "ok"}
+
+
+@app.get("/orders/{identificacao_do_pedido}/items")
+def listar_itens(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
+    return itens
